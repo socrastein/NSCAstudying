1cfc2b804b6b9b684768d0c9d454bf2a53ca4171
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-10 12:19:26 -0700

    Change compile location to root folder for pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, './'),
     filename: "bundle.js",
   },
   module: {
