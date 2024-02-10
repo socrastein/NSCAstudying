cc821681424c63888b0bbd982565f70c847dc2a6
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-10 11:50:15 -0700

    Copy first chapter from NSCA textbook for initial testing

---
+++ src/studyQuestions/questions.js
@@ -0,0 +1,63 @@
+const chapter1 = {
+    ChapterNumber: 1,
+    ChapterTitle: "Structure and Function of the Muscular, Nervous and Skeletal Systems",
+    Questions: [
+        {
+            Q: "Aerobic ATP production within the cell occurs in which of the following cellular organelles?",
+            Answers: [
+                "mitochondria",
+                "sarcoplasmic reticulum",
+                "myofibril",
+                "T-tubule"
+            ],
+            A: 0 //A
+            
+        },
+        {
+            Q: "Which of the following is the thick myofilament?",
+            Answers: [
+                "actin",
+                "troponin",
+                "myosin",
+                "tropomyosin"
+            ],
+            A: 2 //C
+            
+        },
+        {
+            Q: "Which of the following types of muscle action occurs when the muscle lengthens despite attempting to shorten?",
+            Answers: [
+                "concentric",
+                "eccentric",
+                "isometric",
+                "isokinetic"
+            ],
+            A: 1 //B
+            
+        },
+        {
+            Q: "Which of the following muscle fiber types has moderate oxidative and anaerobic capacity?",
+            Answers: [
+                "SO",
+                "FOG",
+                "FG",
+                "slow-twitch"
+            ],
+            A: 1 //B
+            
+        },
+        {
+            Q: "Which of the following branches of the nervous system is responsible for activating skeletal muscles?",
+            Answers: [
+                "sensory",
+                "autonomic",
+                "afferent",
+                "somatic"
+            ],
+            A: 3 //D
+            
+        },
+    ]
+}
+
+export const chapters = [chapter1];
