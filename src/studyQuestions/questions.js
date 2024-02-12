5d725af1f1da835df40adf7c3c126a888e3f15ce
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-11 18:36:55 -0700

    Add chapters 2-5 questions

--- src/studyQuestions/questions.js
+++ src/studyQuestions/questions.js
@@ -1,63 +1,319 @@
 const chapter1 = {
-    ChapterNumber: 1,
-    ChapterTitle: "Structure and Function of the Muscular, Nervous and Skeletal Systems",
-    Questions: [
-        {
-            Q: "Aerobic ATP production within the cell occurs in which of the following cellular organelles?",
-            Answers: [
-                "mitochondria",
-                "sarcoplasmic reticulum",
-                "myofibril",
-                "T-tubule"
-            ],
-            A: 0 //A
-            
-        },
-        {
-            Q: "Which of the following is the thick myofilament?",
-            Answers: [
-                "actin",
-                "troponin",
-                "myosin",
-                "tropomyosin"
-            ],
-            A: 2 //C
-            
-        },
-        {
-            Q: "Which of the following types of muscle action occurs when the muscle lengthens despite attempting to shorten?",
-            Answers: [
-                "concentric",
-                "eccentric",
-                "isometric",
-                "isokinetic"
-            ],
-            A: 1 //B
-            
-        },
-        {
-            Q: "Which of the following muscle fiber types has moderate oxidative and anaerobic capacity?",
-            Answers: [
-                "SO",
-                "FOG",
-                "FG",
-                "slow-twitch"
-            ],
-            A: 1 //B
-            
-        },
-        {
-            Q: "Which of the following branches of the nervous system is responsible for activating skeletal muscles?",
-            Answers: [
-                "sensory",
-                "autonomic",
-                "afferent",
-                "somatic"
-            ],
-            A: 3 //D
-            
-        },
-    ]
-}
+  ChapterNumber: 1,
+  ChapterTitle:
+    "Structure and Function of the Muscular, Nervous and Skeletal Systems",
+  Questions: [
+    {
+      Q: "Aerobic ATP production within the cell occurs in which of the following cellular organelles?",
+      Answers: [
+        "mitochondria",
+        "sarcoplasmic reticulum",
+        "myofibril",
+        "T-tubule",
+      ],
+      A: 0, //A
+    },
+    {
+      Q: "Which of the following is the thick myofilament?",
+      Answers: ["actin", "troponin", "myosin", "tropomyosin"],
+      A: 2, //C
+    },
+    {
+      Q: "Which of the following types of muscle action occurs when the muscle lengthens despite attempting to shorten?",
+      Answers: ["concentric", "eccentric", "isometric", "isokinetic"],
+      A: 1, //B
+    },
+    {
+      Q: "Which of the following muscle fiber types has moderate oxidative and anaerobic capacity?",
+      Answers: ["SO", "FOG", "FG", "slow-twitch"],
+      A: 1, //B
+    },
+    {
+      Q: "Which of the following branches of the nervous system is responsible for activating skeletal muscles?",
+      Answers: ["sensory", "autonomic", "afferent", "somatic"],
+      A: 3, //D
+    },
+  ],
+};
 
-export const chapters = [chapter1];
+const chapter2 = {
+  ChapterNumber: 2,
+  ChapterTitle: "Cardiorespiratory System and Gas Exchange",
+  Questions: [
+    {
+      Q: "Which of the following is the correct sequence of structures that the blood travels through?",
+      Answers: [
+        "superior vena cava, right atrium, left atrium, left ventricle, aorta",
+        "inferior vena cava, right atrium, right ventricle, pulmonary vein, left atrium",
+        "pulmonary vein, pulmonary artery, left ventricle, left atrium, aorta",
+        "superior vena cava, aorta, left atrium, right ventricle",
+      ],
+      A: 1, //B
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following are components of the Fick equation for oxygen uptake (V̇O₂)?\nI. heart rate\nII.systolic pressure\nIII. stroke volume\nIV. diastolic pressure",
+      Answers: [
+        "I and III only",
+        "I and IV only",
+        "II and III only",
+        "II and IV only",
+      ],
+      A: 0, //A
+    },
+  ],
+  Questions: [
+    {
+      Q: "Gas exchange in the lungs occurs at which of the following sites?",
+      Answers: ["capillary wall", "basement membrane", "bronchi", "alveoli"],
+      A: 3, //D
+    },
+  ],
+  Questions: [
+    {
+      Q: "Cardiac output (Q̇) is a product of stroke volume (SV) and heart rate (HR). Which of the following equations represents stroke volume?",
+      Answers: ["V̇O₂ - HR", "V̇O₂ - SV", "EDV - HR", "EDV - ESV"],
+      A: 3, //D
+    },
+  ],
+  Questions: [
+    {
+      Q: "The heart's conduction pathway begins at which of the following sites?",
+      Answers: ["AV node", "Purkinje fibers", "right ventrical", "SA node"],
+      A: 3, //D
+    },
+  ],
+};
+
+const chapter3 = {
+  ChapterNumber: 3,
+  ChapterTitle: "Bioenergetics",
+  Questions: [
+    {
+      Q: "Which of the following describes the process of fast glycolysis?\nI. a breakdown of glycogen\nII. its end product is converted to lactate\nIII. a breakdown of glucose\nIV. its end product is sent to the Krebs cycle",
+      Answers: [
+        "I, II, and III only",
+        "I, II, and IV only",
+        "II, III, and IV only",
+        "I, III, and IV only",
+      ],
+      A: 0, //A
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following describes what a client would be doing to allow the oxidative system to contribute the greatest percentage toward total ATP production?",
+      Answers: ["sitting quietly", "walking", "jogging", "sprinting"],
+      A: 0, //A
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following energy systems is capable of producing the greatest amount (capacity) of ATP?",
+      Answers: [
+        "phosphagen",
+        "fast glycolysis",
+        "slow glycolysis",
+        "oxidative",
+      ],
+      A: 3, //D
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following energy systems is capable of producing ATP at the greatest rate?",
+      Answers: [
+        "phosphagen",
+        "fast glycolysis",
+        "slow glycolysis",
+        "oxidative",
+      ],
+      A: 0, //A
+    },
+  ],
+};
+
+const chapter4 = {
+  ChapterNumber: 4,
+  ChapterTitle: "Biomechanics",
+  Questions: [
+    {
+      Q: "Which of the following exercise modes requires the client to produce the most power?",
+      Answers: [
+        "a 60kg weight lifted in 1 second",
+        "a 60kg weight lifted in 0.1 seconds",
+        "a 30 kg weight lifted in 1 second",
+        "a 30 kg weight lifted in 0.1 seconds",
+      ],
+      A: 1, //B
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following is the best example of open chain exercise?",
+      Answers: ["standing barbell curl", "push-up", "squat", "pull-up"],
+      A: 0, //A
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following changes will increase concentric force production?",
+      Answers: [
+        "decreased rate coding",
+        "decreased leverage over the joint range of motion",
+        "increased contraction velocity",
+        "increased physiological cross-sectional area",
+      ],
+      A: 3, //D
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following best describes the muscular control of the downward movement phase of the squat exercise?",
+      Answers: [
+        "the hip extensors, knee extensors, and ankle plantarflexors acting concentrically",
+        "the hip flexors, knee flexors, and ankle dorsiflexors acting concentrically",
+        "the hip extensors, knee extensors, and ankle plantarflexors acting eccentrically",
+        "the hip flexors, knee flexors, and ankle dorsiflexors acting eccentrically",
+      ],
+      A: 2, //C
+    },
+  ],
+};
+
+const chapter5 = {
+  ChapterNumber: 5,
+  ChapterTitle: "Resistance Training Adaptations",
+  Questions: [
+    {
+      Q: "Which of the following is most likely to occur during muscular fatigue?\nI. motor unit recruitment increases \nII. rate coding increases \nIII. muscle pH decreases \nIV. ATP stores increase",
+      Answers: [
+        "I and II only",
+        "II and IV only",
+        "I and IV only",
+        "II and III only",
+      ],
+      A: 0, //A
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following is primarily responsible for activation of fast-twitch motor units?",
+      Answers: [
+        "increased speed demands",
+        "increased force demands",
+        "decreased speed demands",
+        "decreased force demands",
+      ],
+      A: 1, //B
+    },
+  ],
+  Questions: [
+    {
+      Q: "Which of the following are the most influential training factors promoting increased hormonal responses?",
+      Answers: [
+        "higher volume, shorter rest periods, smaller muscle mass exercises",
+        "higher volume, longer rest periods, larger muscle mass exercises",
+        "higher volume, shorter rest periods, larger muscle mass exercises",
+        "lower volume, shorter rest periods, larger muscle mass exercises",
+      ],
+      A: 2, //C
+    },
+  ],
+  Questions: [
+    {
+      Q: "Resistance exercise augments aerobic endurance performance by which of the following?",
+      Answers: [
+        "increased V̇O₂",
+        "increased oxidative enzyme activity",
+        "increased capillary density",
+        "increased muscular strength and power",
+      ],
+      A: 3, //D
+    },
+  ],
+};
+
+const chapter6 = {
+  ChapterNumber: 6,
+  ChapterTitle:
+    "Physiological Responses and Adaptations to Aerobic Endurance Training",
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+};
+
+const chapter7 = {
+  ChapterNumber: 7,
+  ChapterTitle: "Cardiorespiratory System and Gas Exchange",
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+  Questions: [
+    {
+      Q: "",
+      Answers: ["", "", "", ""],
+      A: 0,
+    },
+  ],
+};
+
+export const chapters = [chapter1, chapter2, chapter3, chapter4, chapter5];
