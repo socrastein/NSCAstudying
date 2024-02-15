40a63373a8e1b15ea3c9a0d6a0e9d8fac3629fe5
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-14 17:33:00 -0700

    Reformat to include specific chapters & toggle randomization

--- src/studyQuestions/generateQuestions.js
+++ src/studyQuestions/generateQuestions.js
@@ -1,31 +1,58 @@
 import { chapters } from "./questions";
 
-export const newQuestionGenerator = () => {
-  const chaptersArray = structuredClone(chapters);
+export const newQuestionGenerator = (selectedChapters = [], random = false) => {
+  let chaptersArray = [];
+  const length = selectedChapters.length;
 
-  const getRandomQuestion = () => {
-    console.log(chaptersArray);
+  if(length === 0){
+    chaptersArray = structuredClone(chapters); //All chapters selected
+  } else {
+    for (let i = 0; i < length; i++){ //Some chapters selected
+      const chapterIndex = selectedChapters[i];
+      chaptersArray.push(structuredClone(chapters[chapterIndex]))
+    }
+  }
+
+  const getNextQuestion = () => {
     while (chaptersArray.length > 0) {
-      let randomChapter = pickRandomElement(chaptersArray);
 
-      if (randomChapter.Questions.length > 0) {
-        //Pull random question out of the Questions array from selected chapter
-        const chapterNumber = randomChapter.ChapterNumber;
-        const chapterTitle = randomChapter.ChapterTitle;
-        const questionObj = removeRandomElement(randomChapter.Questions);
-        return { chapterNumber, chapterTitle, questionObj };
+      let chapter;
+      if(random){
+        chapter = pickRandomElement(chaptersArray);
       } else {
-        //Remove selected chapter from array if all the questions have already been pulled
+        chapter = chaptersArray[0];
+      }
+
+      if(chapter.Questions.length === 0){
+        if(random){
         let index = chaptersArray.indexOf(randomChapter);
         chaptersArray.splice(index, 1);
+        } else {
+        chaptersArray.shift();
+        }
+      } else {
+        //Pull next question out of the Questions array from selected chapter
+        return getNextChapterQuestion(chapter, random)
       }
     }
     //If all the chapters' questions have already been pulled
     return null;
-  };
+  }
 
-  return { getRandomQuestion };
+  return { getNextQuestion };
 };
+
+const getNextChapterQuestion = (chapter, random = false) => {
+  const chapterNumber = chapter.ChapterNumber;
+  const chapterTitle = chapter.ChapterTitle;
+  let questionObj;
+  if(random){
+    questionObj = removeRandomElement(chapter.Questions);
+  } else {
+    questionObj = chapter.Questions.shift();
+  }
+  return { chapterNumber, chapterTitle, questionObj };
+}
 
 const pickRandomElement = (arr) => {
   const randomElement = arr[Math.floor(Math.random() * arr.length)];
