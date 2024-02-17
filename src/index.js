67b3a93acc0d7ae89c420fb6247a1ceec4e5d952
socrastein <31764357+socrastein@users.noreply.github.com>
2024-02-16 19:20:08 -0700

    Add footer to document

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./style.css";
 import { loadStartMenu } from "./studyQuestions/startMenu";
+import { createFooter } from "./footer/footer";
 
 loadStartMenu();
+
+document.body.append(createFooter())
